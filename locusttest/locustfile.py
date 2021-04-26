42db2387ca2097ce4867fee42704d06799a21f4e
KyleBarts <kpibartido@gmail.com>
2021-04-26 13:21:44 +0800

    added locustfile 1 day

--- locusttest/locustfile.py
+++ locusttest/locustfile.py
@@ -5,6 +5,10 @@ class QuickstartUser(HttpUser):
     wait_time = between(1, 2.5)
 
     @task
+    def vpoteka_filtered1day(self):
+        self.client.get("/vpoteka?start_date=2020-05-27 18:30:00&end_date=2020-06-27 18:29:59")
+
+    @task
     def vpoteka_filtered3hr(self):
         self.client.get("/vpoteka?start_date=2020-05-27 18:30:00&end_date=2020-05-27 21:29:59")
 
