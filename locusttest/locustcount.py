325733c108311e4756a6212671456310e1e6dbc5
KyleBarts <kpibartido@gmail.com>
2021-04-26 13:32:50 +0800

    Added count file

---
+++ locusttest/locustcount.py
@@ -0,0 +1,46 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def ppoteka_filtered1day(self):
+        self.client.get("/ppoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-06-27 18:29:59")
+
+    @task
+    def ppoteka_filtered12hr(self):
+        self.client.get("/ppoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-06-27 6:29:59")
+
+    @task
+    def ppoteka_filtered6hr(self):
+        self.client.get("/ppoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-05-27 23:29:59")
+    
+    @task
+    def ppoteka_filtered3hr(self):
+        self.client.get("/ppoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-05-27 21:29:59")
+
+    @task
+    def ppoteka_filtered1hr(self):
+        self.client.get("/ppoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-05-27 19:29:59")
+
+    @task
+    def vpoteka_filtered1day(self):
+        self.client.get("/vpoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-06-27 18:29:59")
+
+    @task
+    def vpoteka_filtered12hr(self):
+        self.client.get("/vpoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-06-27 6:29:59")
+
+    @task
+    def vpoteka_filtered6hr(self):
+        self.client.get("/vpoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-05-27 23:29:59")
+
+    @task
+    def vpoteka_filtered3hr(self):
+        self.client.get("/vpoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-05-27 21:29:59")
+
+    @task
+    def vpoteka_filtered1hr(self):
+        self.client.get("/vpoteka/count?start_date=2020-05-27 18:30:00&end_date=2020-05-27 19:29:59")
+
--- locusttest/locustfile.py
+++ locusttest/locustfile.py
@@ -9,6 +9,14 @@ class QuickstartUser(HttpUser):
         self.client.get("/vpoteka?start_date=2020-05-27 18:30:00&end_date=2020-06-27 18:29:59")
 
     @task
+    def vpoteka_filtered12hr(self):
+        self.client.get("/vpoteka?start_date=2020-05-27 18:30:00&end_date=2020-06-27 6:30:30")
+
+    @task
+    def vpoteka_filtered6hr(self):
+        self.client.get("/vpoteka?start_date=2020-05-27 18:30:00&end_date=2020-05-27 23:29:59")
+
+    @task
     def vpoteka_filtered3hr(self):
         self.client.get("/vpoteka?start_date=2020-05-27 18:30:00&end_date=2020-05-27 21:29:59")
 
